439e5b884a85446efaf8ee54d46471b2fb2a90a1
empg-talha <talha.khan@empglabs.com>
2022-11-16 13:07:37 +0500

    Project Base

---
+++ Uni-List-App/Uni-List-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uni-List-App
+//
+//  Created by Talha Ahmad on 16/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+
+
+}
+
